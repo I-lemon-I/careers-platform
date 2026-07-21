b3cd0d873599b1615e01f148a33daa9e7abf0f64
I-lemon-I <marynam2025@gmail.com>
2026-07-21 22:02:14 +0100

    Add jobs API routes

---
+++ backend/src/api/routes/jobs.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const jobsController = require('../contorollers/jobsController');
+const { authenticate, validateJob } = require('../middleware');
+
+router.get('/', jobsController.list);
+router.get('/:id', jobsController.getById);
+router.post('/', authenticate, validateJob, jobsController.create);
+router.put('/:id', authenticate, validateJob, jobsController.update);
+router.delete('/:id', authenticate, jobsController.delete);
+
+module.exports = router;
